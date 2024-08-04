51aa936b2b63008cdb07c9040b1f9a9cc09a49b6
phong ngô văn <n.v.p200200@gmail.com>
2024-08-04 22:50:12 +0700

    Reapply "first commit"
    This reverts commit e3ab7fca84ba6c01dc13373a5a4fa8e4e591f1b7.

---
+++ lib/widget/WidgetItemRoom.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter_ui/model/room.dart';
+import "package:flutter/material.dart";
+
+Widget itemRoom(room item) {
+  return Container(
+    margin: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.grey,
+      image: DecorationImage(
+        image: AssetImage('assets/images/room01.png'),
+        fit: BoxFit.cover, 
+      ),
+    ),
+    child: Center(
+      child: Text('Your Content Here'),
+    ),
+  );
+}
